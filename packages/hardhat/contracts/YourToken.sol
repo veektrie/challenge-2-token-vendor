a04d40e3e3503e49146e7d4564258df1f0f47cfd
adugbo victory <adugbovictory@gmail.com>
2025-02-20 10:31:19 +0100

    remove linting workflows and husky pre-commit hook; update YourToken contract and deployedContracts with new ABI

--- packages/hardhat/contracts/YourToken.sol
+++ packages/hardhat/contracts/YourToken.sol
@@ -3,10 +3,10 @@ pragma solidity 0.8.20; //Do not change the solidity version as it negatively im
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-// learn more: https://docs.openzeppelin.com/contracts/4.x/erc20
+// Learn more: https://docs.openzeppelin.com/contracts/4.x/erc20
 
 contract YourToken is ERC20 {
     constructor() ERC20("Gold", "GLD") {
-        //_mint( ~~~YOUR FRONTEND ADDRESS HERE~~~~ , 1000 * 10 ** 18);
+        _mint(0x994A5710889eeA17999B7c86D3d6b6c21ab53C19, 1000 * 10 ** 18);
     }
 }
