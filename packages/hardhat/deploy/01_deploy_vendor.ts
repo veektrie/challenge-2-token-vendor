ceb607c3241aa2b867de58e8dbef8d30be3667cc
adugbo victory <adugbovictory@gmail.com>
2025-02-21 15:25:07 +0100

    update YourToken and vendor deployment script; change token minting address and update ownership transfer address

--- packages/hardhat/deploy/01_deploy_vendor.ts
+++ packages/hardhat/deploy/01_deploy_vendor.ts
@@ -35,7 +35,7 @@ const deployVendor: DeployFunction = async function (hre: HardhatRuntimeEnvironm
 
   // (Optional) Transfer contract ownership to your frontend address
   // Replace **YOUR FRONTEND ADDRESS** with your actual frontend address if needed.
-  await vendor.transferOwnership("0x994A5710889eeA17999B7c86D3d6b6c21ab53C19");
+  await vendor.transferOwnership("0xA51c1fc2f0D1a1b8494Ed1FE312d7C3a78Ed91C0");
 };
 
 export default deployVendor;
